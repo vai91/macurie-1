24e42b29aa57a089a4ea934f0ef8f911d92a530c
Sarpt <mike.m.info@gmail.com>
2017-06-19 23:44:26 +0200

    Added frontend tests, problems with Karma resolved

--- tests/frontend/register.controller.spec.js
+++ tests/frontend/register.controller.spec.js
@@ -1,8 +1,7 @@
-describe('Test in Login controller that', () => {
+describe('Test in Register controller that', () => {
   beforeEach(angular.mock.module('macurieModule'));
 
   let $controller,
-    $userService,
     $httpBackend,
     $apiService,
     $location,
@@ -12,12 +11,11 @@ describe('Test in Login controller that', () => {
   beforeEach(() => {
     angular.mock.inject(($injector) => {
       $controller = $injector.get('$controller');
-      $userService = $injector.get('$userService');
       $httpBackend = $injector.get('$httpBackend');
       $apiService = $injector.get('$apiService');
       $location = $injector.get('$location');
       $scope = {};
-      controller = $controller('loginController', { $scope, $userService, $apiService, $location });
+      controller = $controller('registerController', { $scope, $apiService, $location });
     });
   });
 
@@ -32,11 +30,16 @@ describe('Test in Login controller that', () => {
     it('are expected during successful request to be true and message to be properly set', () => {
       const userInfo = {
         email: 'mike@mike',
-        password: 'mikemike',
+        password: '',
+        name: 'mike',
+        surname: 'mike',
+        nick: 'mike',
+        address: 'address',
+        role: 'author',
       };
 
-      $httpBackend.when('POST', '/api/user/login', userInfo)
-        .respond(201, { token: 'token', info: {} });
+      $httpBackend.when('POST', '/api/user/register', userInfo)
+        .respond(201, { message: 'Registration successful' });
       
       $scope.user = userInfo;
       $scope.submit(true);
@@ -44,16 +47,21 @@ describe('Test in Login controller that', () => {
       $httpBackend.flush();
       expect($scope.showSuccess).toEqual(true);
       expect($scope.showFailure).toEqual(false);
-      expect($scope.message).toEqual('Login Successful');
+      expect($scope.message).toEqual('Registration successful');
     });
 
     it('are expected during successful request to be true and message to be properly set', () => {
-      const userInfo = { 
+      const userInfo = {
         email: 'mike@mike',
         password: '',
+        name: 'mike',
+        surname: 'mike',
+        nick: 'mike',
+        address: 'address',
+        role: 'author',
       };
-      $httpBackend.when('POST', '/api/user/login', userInfo)
-        .respond(401, { message: 'Wrong password' });
+      $httpBackend.when('POST', '/api/user/register', userInfo)
+        .respond(401, { message: 'Email not unique' });
       
       $scope.user = userInfo;
       $scope.submit(true);
@@ -61,7 +69,7 @@ describe('Test in Login controller that', () => {
       $httpBackend.flush();
       expect($scope.showFailure).toEqual(true);
       expect($scope.showSuccess).toEqual(false);
-      expect($scope.message).toEqual('Wrong password');
+      expect($scope.message).toEqual('Email not unique');
     });
   });
 });
